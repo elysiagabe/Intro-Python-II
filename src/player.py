89ca028a0fe05a9342c4a37dc4aef29f2fd985e0
Elysia Gabe <elysiagabe@gmail.com>
2020-07-09 15:51:54 -0700

    Adds item class and MVP functionality plus stretch (adds a way to win the game)

--- src/player.py
+++ src/player.py
@@ -4,6 +4,15 @@ class Player:
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
+        self.inventory = []
+
+    def on_take(self, item):
+        self.inventory.append(item)
+        print("You have picked up the ",{item})
+
+    def on_drop(self, item):
+        self.inventory.remove(item)
+        print("You have dropped the ",{item})
 
     def __str__(self):
         return f"Player Name: {self.name}, Current Room: {self.current_room}"
--- src/room.py
+++ src/room.py
@@ -1,9 +1,16 @@
+from item import Item
 # Implement a class to hold room information. This should have name and
 # description attributes.
 class Room: 
     def __init__(self, name, description):
         self.name = name
         self.desc = description
+        self.items = []
+
+    def add_item(self, item):
+        print(item)
+        self.items.append(item.name) #TODO: decide if I want the whole item object here or if just the name is sufficient
 
     def __str__(self):
-        return f"You are currently located in the {self.name}. {self.desc}"
+        return f"\nYou are currently located in the {self.name}. {self.desc}\nItems here: {self.items}"
+        #TODO: look into parsing out items so it doesn't display brackets and single quote marks
